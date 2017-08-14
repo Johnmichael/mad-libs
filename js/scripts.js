f858c9183c63f6a4320a691f3be10bc847e9bf91
John Parsons and Riki Montgomery <riki@>
2017-08-14 13:26:47 -0700

    add loop js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
 $(document).ready(function() {
-  $("#formOne").submit(function() {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+  // $("#formOne").submit(function() {
+  $("#blankForm").submit(function(event) {
+    var blankForms = ['person1', 'person2', 'animal', 'exclamation', 'verb', 'noun'];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    blankForms.forEach(function(form) {
+      var userInput = $("input#" + form).val();
+      $("." + form).text(userInput);
+    });
+
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput= $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
 
     $("#story").show();
 
     event.preventDefault();
   });
+  // });
 });
